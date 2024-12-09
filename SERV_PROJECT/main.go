4509c86f54cf7c7201c7c09a69a764fc0a040541
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2024-12-10 01:12:05 +0300

    update

--- SERV_PROJECT/main.go
+++ SERV_PROJECT/main.go
@@ -1,9 +1,50 @@
 package main
 
-import(
+import (
 	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+type Product struct {
+	ID int
+	NAME string
+}
+
+var products = []Product{{1, "CocaCola"}, {2, "Pepsi"}}
+
 func main(){
-	fmt.Println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Println("HELLO")
+	})
+	r.HandleFunc("/ProductsName/{key}", ProductHandlerName)
+	r.HandleFunc("/ProductsID/{key}", ProductHandlerId)
+	http.ListenAndServe(":8080", r)
+}
+
+
+func ProductHandlerName(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	name := vars["key"]
+	for elem := range products{
+		if products[elem].NAME == name{
+			fmt.Printf("Продукт под номером %d\n", products[elem].ID)
+		}
+	}
+}
+
+func ProductHandlerId(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["key"])
+	if err != nil{
+		fmt.Println(err)
+	}
+	for elem := range products{
+		if products[elem].ID == id{
+			fmt.Printf("Продукт называется %s\n", products[elem].NAME)
+		}
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module server/project_golang
+
+go 1.23.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
